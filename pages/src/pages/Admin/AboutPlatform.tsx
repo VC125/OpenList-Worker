1827e0d0dd1e25e1cf884c6614b6954b359a9fdf
VC <2723202745@qq.com>
2026-07-08 13:17:21 +0800

    Update AboutPlatform.tsx

--- pages/src/pages/Admin/AboutPlatform.tsx
+++ pages/src/pages/Admin/AboutPlatform.tsx
@@ -17,7 +17,7 @@ import {
   TeamOutlined,
   DatabaseOutlined,
 } from '@ant-design/icons';
-import { systemApi } from '../../posts/api';
+import { Apisystem } from '../../posts/api';
 
 interface SystemInfo {
   version?: string;
